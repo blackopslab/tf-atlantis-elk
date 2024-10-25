724364abeee4979b1fcb5d57fa57f49791be05c6
Vittorio Distefano <info@vdistefano.studio>
2024-10-25 16:43:08 +0200

    feat: Refactor opensearch to elk
    - Rename `deploy/opensearch` directory to `deploy/elk`
    - Update paths in Makefile and atlantis.yaml to reflect new directory name
    - Rename helm values files under the new `deploy/elk/helm` directory
    - Modify main.tf under `deploy/elk/terraform` to use renamed helm values files and add a new resource for opensearch with prometheus values

---
+++ deploy/elk/terraform/main.tf
@@ -0,0 +1,23 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "opensearch" {
+  name       = "opensearch"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "opensearch"
+  namespace  = "monitoring"
+
+  values = [file("${path.module}/../helm/opensearch.yaml")]
+}
+
+resource "helm_release" "opensearch" {
+  name       = "opensearch"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "opensearch"
+  namespace  = "monitoring"
+
+  values = [file("${path.module}/../helm/prometheus.yaml")]
+}
