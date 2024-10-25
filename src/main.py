3554e177b02ab0d5d39c0dcafc15bfdd3f8594db
Vittorio Distefano <info@vdistefano.studio>
2024-10-25 20:59:06 +0200

    refactor: Refactor main.py and install.py for better error handling and code readability
    - Renamed functions in main.py to more descriptive names
    - Removed verbose mode from main.py commands
    - Updated version number in main.py
    - Added detailed docstrings to functions in install.py
    - Improved error handling in install.py with more specific exceptions
    - Added new helper functions in install.py for creating directories, setting permissions, etc.
    - Changed function visibility (from private to public) for Terraform operations in install.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys, click
-from util.install import t_install as env_install
-from util.install import t_apply as env_apply
-from util.install import t_destroy as env_destroy
+from util.install import install_atlantis
+from util.install import run_terraform_apply
+from util.install import run_terraform_destroy
 
 
 @click.group()
@@ -13,59 +13,33 @@ def main():
 
 
 @main.command()
-@click.option("--verbose", "-v", is_flag=True, help="Prints installation output.")
 def install(verbose):
     """Installs Atlantis from GitHub credentials provided by terraform/variables.tfvars.
 
     Example:
-        python main.py install --verbose
+        python main.py install
     """
-    match verbose:
-        case True:
-            click.echo("Verbose mode is enabled.")
-            click.echo(env_install())
-
-        case False:
-            click.echo("Verbose mode is disabled.")
-            env_install()
+    click.echo(install_atlantis())
 
 
 @main.command()
-@click.option("--verbose", "-v", is_flag=True, help="Prints 'terraform apply' output.")
 def apply(verbose):
     """Creates all Terraform resources in ./terraform/
 
     Example:
-        python main.py apply --verbose
+        python main.py apply
     """
-    match verbose:
-        case True:
-            click.echo("Verbose mode is enabled.")
-            click.echo(env_apply())
-
-        case False:
-            click.echo("Verbose mode is disabled.")
-            env_apply()
+    click.echo(run_terraform_apply())
 
 
 @main.command()
-@click.option(
-    "--verbose", "-v", is_flag=True, help="Prints 'terraform destroy' output."
-)
 def destroy(verbose):
     """Destroys all Terraform resources in ./terraform/
 
     Example:
-        python main.py destroy --verbose
+        python main.py destroy
     """
-    match verbose:
-        case True:
-            click.echo("Verbose mode is enabled.")
-            click.echo(env_destroy())
-
-        case False:
-            click.echo("Verbose mode is disabled.")
-            env_destroy()
+    click.echo(run_terraform_destroy())
 
 
 @main.command()
@@ -75,13 +49,16 @@ def version():
     Example:
         python main.py version
     """
-    click.echo("tf-atlantis-cli version: 3.0.0")
+    click.echo("tf-atlantis-cli version: 4.0.0")
 
 
 if __name__ == "__main__":
     # show help if no args
     if len(sys.argv) == 1:
         click.echo("No command provided. Use one of the following:")
+        click.echo("  install             Full installation script.")
+        click.echo("  apply               Apply Terraform manifests.")
+        click.echo("  destroy             Destroy Terraform manifests.")
         click.echo("  version             Show the version of the CLI tool.")
         click.echo("\nFor more information, run 'python main.py <command> --help'.")
     else:
--- src/util/install.py
+++ src/util/install.py
@@ -8,65 +8,109 @@ def _run_command(command: str) -> str:
         cmd = shlex.split(command)
         output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         return output.decode("utf-8")
-    except subprocess.CalledProcessError as e:
+
+    except subprocess.CalledProcessError as called_process_error:
         raise RuntimeError(
-            f"Command '{command}' failed with error: {e.output.decode('utf-8')}"
+            f"Command '{command}' failed with error: {called_process_error.output.decode('utf-8')}"
         )
 
 
 def _change_working_directory(path: str) -> None:
+    """
+    Changes the current working directory to the specified path.
+    TODO: deal with permission_error
+    """
+
     try:
         os.chdir(path)
-    except FileNotFoundError:
+        print(f"Current working directory: {os.getcwd()}")
+
+    except FileNotFoundError as file_not_found_error:
         print(f"Directory {path} not found.")
-        raise
-    except NotADirectoryError:
+        raise file_not_found_error
+
+    except NotADirectoryError as not_a_directory_error:
         print(f"{path} is not a directory.")
-        raise
-    except PermissionError:
-        print(f"Permission denied to access {path}.")
-        raise
+        raise not_a_directory_error
+
+    except PermissionError as permission_error:
+        raise RuntimeError(f"Permission denied to access {path}: {permission_error}")
+
+
+def _create_directory(path: str) -> None:
+    """
+    Creates a directory at the specified path if it doesn't already exist.
+    TODO: deal with permission_error
+    """
+    try:
+        os.makedirs(path, exist_ok=True)
+
+    except PermissionError as permission_error:
+        raise RuntimeError(f"Failed to create required directories: {permission_error}")
 
 
 def _download_binary(url: str, output_path: str) -> None:
+    """
+    Downloads a binary from the specified URL and saves it to the output path.
+    """
     try:
         if os.path.exists(output_path):
             print(f"{output_path} already exists!")
             return
         else:
             _run_command(f"wget {url} -O {output_path}")
-    except OSError:
-        print(f"Error downloading {url} to {output_path}")
-        raise
+
+    except OSError as download_error:
+        raise RuntimeError(
+            f"Error downloading {url} to {output_path}: {download_error}"
+        )
 
 
 def _unzip_binary(zip_path: str, extract_to: str) -> None:
+    """
+    Unzips a zip file to the specified directory.
+    """
     try:
         _run_command(f"unzip {zip_path} -d {extract_to}")
-    except OSError:
+
+    except PermissionError:
         _set_permissions(extract_to, "755")
+
         try:
             _run_command(f"unzip {zip_path} -d {extract_to}")
+
         except OSError as e:
-            print(f"Failed to unzip {zip_path} again after setting permissions.")
-            raise e
+            raise RuntimeError(
+                f"Failed to unzip {zip_path} again after setting permissions: {e}"
+            )
 
 
 def _untar_binary(tar_path: str, extract_to: str) -> None:
+    """
+    Untars a tar.gz file to the specified directory.
+    """
     try:
         _run_command(f"tar -xvf {tar_path} -C {extract_to}")
-    except OSError:
+
+    except PermissionError:
         _set_permissions(extract_to, "755")
+
         try:
             _run_command(f"tar -xvf {tar_path} -C {extract_to}")
+
         except OSError as e:
-            print(f"Failed to untar {tar_path} again after setting permissions.")
-            raise e  # Re-raise the exception to propagate the error
+            raise RuntimeError(
+                f"Failed to untar {tar_path} again after setting permissions: {e}"
+            )
 
 
-def _match_and_extract(name: str, temp_path: str, install_path: str):
+def _match_and_extract(name: str, temp_path: str, install_path: str) -> None:
+    """
+    Matches the file extension of the downloaded binary and extracts it to the specified directory.
+    """
     try:
         _, file_extension = os.path.splitext(temp_path)
+
         match file_extension:
             case ".gz":
                 print(f"Extracting {name} binary from tar.gz...")
@@ -78,52 +122,15 @@ def _match_and_extract(name: str, temp_path: str, install_path: str):
                 print(f"No extension for {name}. Skipping extraction.")
             case _:
                 print(f"Unknown file type for {name}.")
-    except OSError:
-        print(f"Error extracting {name} binary.")
-        raise
 
-
-def _install_binaries(binaries):
-    for name, url, temp_path, install_path, permissions in binaries:
-        try:
-            print(f"Downloading {name} binary...")
-            _download_binary(url, temp_path)
-            _match_and_extract(name, temp_path, install_path)
-            _set_permissions(install_path, permissions)
-
-        except RuntimeError as e:
-            print(f"Error downloading {name} binary: {e}")
-            print(f"URL {url} does not respond!")
-
-        except Exception as e:
-            print(f"Unexpected error installing {name} binary: {e}")
-
-
-def _set_permissions(path: str, mode: str) -> None:
-    _run_command(f"chmod {mode} {path}")
-
-
-def _set_kube_config_path(conf_path: str) -> None:
-    os.environ["KUBE_CONFIG_PATH"] = conf_path
-
-
-def _run_terraform_init() -> None:
-    _run_command("terraform init")
-
-
-def _run_terraform_plan() -> None:
-    _run_command("terraform plan -var-file='variables.tfvars'")
-
-
-def _run_terraform_apply() -> None:
-    _run_command("terraform apply -auto-approve -var-file='variables.tfvars'")
-
-
-def _run_terraform_destroy() -> None:
-    _run_command("terraform destroy -auto-approve -var-file='variables.tfvars'")
+    except OSError as e:
+        raise RuntimeError(f"Failed to extract {name} binary: {e}")
 
 
 def _map_binaries() -> List[Tuple[str, str, str, str, str]]:
+    """
+    Returns a list of tuples containing the name, URL, temporary path, installation path, and permissions for each binary.
+    """
     return [
         (
             "Cloudflared",
@@ -135,55 +142,88 @@ def _map_binaries() -> List[Tuple[str, str, str, str, str]]:
     ]
 
 
-def _terraform_operation(operation: str) -> str:
+def _install_binaries(binaries) -> None:
+    """
+    Iterates over a list of binaries, downloads them, extracts them, and sets permissions.
+    """
+    for name, url, temp_path, install_path, permissions in binaries:
+        try:
+            print(f"Downloading {name} binary...")
+            _download_binary(url, temp_path)
+            _match_and_extract(name, temp_path, install_path)
+            _set_permissions(install_path, permissions)
+
+        except RuntimeError as e:
+            print(f"Error downloading {name} binary.")
+            print(f"URL {url} does not respond!")
+            raise e
+
+        except Exception as e:
+            print(f"Unexpected error installing {name} binary.")
+            raise e
+
+
+def _set_permissions(path: str, mode: str) -> None:
+    """
+    Sets the permissions of a file or directory to the specified mode.
+    TODO: deal with no_sudo_error
+    """
     try:
-        print(f"\n{operation.capitalize()} all Terraform resources...")
-        _set_kube_config_path("~/.kube/config/")
-        _change_working_directory("./deploy/atlantis/terraform/")
+        _run_command(f"chmod {mode} {path}")
 
-        if operation == "apply":
-            _run_terraform_apply()
-        elif operation == "destroy":
-            _run_terraform_destroy()
-
-        _change_working_directory("../../../")
-        return f"'terraform {operation}' completed successfully."
-
-    except Exception as e:
-        return f"Error during 'terraform {operation}': {e}"
+    except PermissionError as no_sudo_error:
+        raise RuntimeError(
+            f"Failed to set permissions for {path}: {no_sudo_error}. Do you have sudo permissions?"
+        )
 
 
-def t_apply() -> str:
-    return _terraform_operation("apply")
+def _set_kube_config_path(conf_path: str) -> None:
+    os.environ["KUBE_CONFIG_PATH"] = conf_path
 
 
-def t_destroy() -> str:
-    return _terraform_operation("destroy")
+def run_terraform_init() -> None:
+    _run_command("terraform init")
 
 
-def t_install() -> str:
+def run_terraform_plan() -> None:
+    _run_command("terraform plan -var-file='variables.tfvars'")
+
+
+def run_terraform_apply() -> None:
+    _run_command("terraform apply -auto-approve -var-file='variables.tfvars'")
+
+
+def run_terraform_destroy() -> None:
+    _run_command("terraform destroy -auto-approve -var-file='variables.tfvars'")
+
+
+def _populate_environment() -> None:
     print("\n1. POPULATING ENVIRONMENT...")
-
     BINARIES = _map_binaries()
-
     try:
         print("Generating required directory tree...")
-        os.makedirs("./bin", exist_ok=True)
-        os.makedirs("./tmp", exist_ok=True)
+        _create_directory("./bin")
+        _create_directory("./tmp")
+        _install_binaries(BINARIES)
 
-    except OSError as e:
-        print(f"Error creating required directories: {e}")
-        return "Installation failed due to directory creation error."
+    except Exception as e:
+        raise e
 
-    _install_binaries(BINARIES)
 
+def install_atlantis() -> str:
     try:
+        _populate_environment()
+
         print("\n2. INSTALLING...")
+
         _set_kube_config_path("~/.kube/config/")
+
         _change_working_directory("./deploy/atlantis/terraform/")
-        _run_terraform_init()
-        _run_terraform_plan()
-        _run_terraform_apply()
+
+        run_terraform_init()
+        run_terraform_plan()
+        run_terraform_apply()
+
         _change_working_directory("../../../")
 
     except Exception as e:
