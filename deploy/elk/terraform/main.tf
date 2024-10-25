926652f860d878a1bec671ef889cdbe6ceb731d4
Vittorio Distefano <info@vdistefano.studio>
2024-10-25 16:46:39 +0200

    chore(release): 3.1.0-alpha.0
    .

--- deploy/elk/terraform/main.tf
+++ deploy/elk/terraform/main.tf
@@ -12,12 +12,3 @@ resource "helm_release" "opensearch" {
 
   values = [file("${path.module}/../helm/opensearch.yaml")]
 }
-
-resource "helm_release" "opensearch" {
-  name       = "opensearch"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "opensearch"
-  namespace  = "monitoring"
-
-  values = [file("${path.module}/../helm/prometheus.yaml")]
-}
